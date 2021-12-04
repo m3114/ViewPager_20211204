de4363e14c82c0d2ecb771925985ff982897422d
윤성노 <m3114@naver.com>
2021-12-04 13:45:25 +0900

    메인화면 뷰페이지 배치= 페리지별 프래ㅔ그면터를 알려주는 adapter 생성

---
+++ app/src/main/java/com/neppplus/viewpager_20211204/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.neppplus.viewpager_20211204.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.viewpager_20211204.fragments.AddressFragment
+import com.neppplus.viewpager_20211204.fragments.AgeFragment
+import com.neppplus.viewpager_20211204.fragments.NameFregment
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 3
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+        if (position == 0){
+            return NameFregment()
+        }
+        else if (position == 1){
+            return AddressFragment()
+        }
+        else{
+            return AgeFragment()
+        }
+
+    }
+
+}
